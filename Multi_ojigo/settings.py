0d5a4850a302d50ce15a5e3f241d2ead11f580bf
kakaocloudschool <cho@naver.com>
2022-12-06 13:04:38 +0900

    scheduler front complete

--- Multi_ojigo/settings.py
+++ Multi_ojigo/settings.py
@@ -98,15 +98,26 @@ WSGI_APPLICATION = "Multi_ojigo.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+pymysql.install_as_MySQLdb()
 if DEBUG:
+    # DATABASES = {
+    #     "default": {
+    #         "ENGINE": "django.db.backends.sqlite3",
+    #         "NAME": BASE_DIR / "db.sqlite3",
+    #     }
+    # }
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": "argocd",
+            "USER": "root",
+            "PASSWORD": "test123",
+            "HOST": "127.0.0.1",
+            "PORT": "3306",
+            "OPTIONS": {"init_command": 'SET sql_mode="STRICT_TRANS_TABLES"'},
         }
     }
-# # else:
-#     pymysql.install_as_MySQLdb()
+# else:
 #     DATABASES = {
 #         "default": {
 #             "ENGINE": "django.db.backends.mysql",
