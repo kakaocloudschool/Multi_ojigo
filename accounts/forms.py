f764183950075fe2d91e1e3145e611a27860f16a
kakaocloudschool <cho@naver.com>
2022-12-08 14:05:01 +0900

    group model complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
@@ -5,3 +6,6 @@ class SignupForm(UserCreationForm):
   class Meta:
     model = CustomUser
     fields = ['username', 'password1', 'password2', 'group', 'privilege']
+    widgets = {
+      'privilege': forms.Select
+    }
--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,12 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    MY_USERS = (  # 각 튜플의 첫 번째 요소는 DB에 저장할 실제 값이고, 두 번째 요소는 display 용 이름이다.
+        ("Manager", "매니저"),
+        ("User", "유저"),
+    )
+
     group = models.CharField(max_length=100)
-    privilege = models.CharField(max_length=100)
+    privilege = models.CharField(
+        max_length=20, choices=MY_USERS, default="User"
+    )
