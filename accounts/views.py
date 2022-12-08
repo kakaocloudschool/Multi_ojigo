abc4db7c9b9ebdecfe7fd11aa5c91147b773fd9d
ksh322 <stevekim97@naver.com>
2022-12-08 14:23:04 +0900

    base.html 알림 +1아이콘

--- accounts/views.py
+++ accounts/views.py
@@ -11,8 +11,8 @@ from .forms import SignupForm
 
 @login_required
 def signup_view(request):
-    if request.user.group != "admin":
-        return redirect("app_list")
+    #if request.user.group != "admin":
+    #    return redirect("app_list")
     if request.method == "POST":
         # print(request.method)
         form = SignupForm(request.POST)
